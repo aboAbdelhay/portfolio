c4d8280e3a7eac6406fe06afc6ed25baf0e4e7f5
ahmed sayed abdelhay <ahmed01090857607@gmail.com>
2024-08-12 20:41:32 +0200

    edit works

--- js/style.js
+++ js/style.js
@@ -1,63 +1,89 @@
+// to go to top when make reload or refresh
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    window.scrollTo(0, 0);
+  }, 0);
+});
+
+//to make wow work
 new WOW().init();
 // -------------------
 // animation to change the words in home
 // -------------------
-var typed = new Typed("#element", {
-  strings: [" I'm Ahmed Sayed Abdelhay^1000 ", " Front End Developer^1000 ", "Framework React.js/Next.js^1000 "],
+let typed = new Typed("#element", {
+  strings: [
+    " I'm Ahmed Sayed Abdelhay^1000 ",
+    " Front End Developer^1000 ",
+    "Framework React.js/Next.js^1000 ",
+  ],
   typeSpeed: 30,
   backSpeed: 30,
   loop: true,
 });
 //  --------------------
 // make btn in bottom when clink scroll to top navbar
-const up = document.getElementById("up");
+document.addEventListener("DOMContentLoaded", function () {
+  const up = document.getElementById("up");
+  const homeSection = document.querySelector("#home"); // Assuming the home section has an id="home"
 
-window.addEventListener("scroll", () => {
-  if (
-    document.body.scrollTop > 100 ||
-    document.documentElement.scrollTop > 100
-  ) {
-    up.classList.remove("hidden");
-  } else {
-    up.classList.add("hidden");
-  }
-});
-// Scroll to top when button is clicked
-up.addEventListener("click", () => {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  window.addEventListener("scroll", () => {
+    // Get the bottom position of the home section
+    const homeSectionBottom = homeSection.offsetTop + homeSection.offsetHeight;
+
+    // Show the "up" button after scrolling past the home section
+    if (window.scrollY > homeSectionBottom) {
+      up.classList.remove("hidden");
+    } else {
+      up.classList.add("hidden");
+    }
+  });
+
+  // Scroll to the top when the button is clicked
+  up.addEventListener("click", () => {
+    window.scrollTo(0, 0);
+  });
 });
 
 // -----------------------
 // code when to scroll add class active
 // --------------------------
 window.addEventListener("scroll", function () {
-  var sections = document.querySelectorAll(".nvscroll");
-  var navbarLinks = document.querySelectorAll("#navbar .navbar-nav .nav-link ");
+  let sections = document.querySelectorAll("section");
+  let navbarLinks = document.querySelectorAll("#navbar .navbar-nav .nav-link");
+
+  let currentIndex = -1;
 
   sections.forEach(function (section, index) {
-    var sectionTop = section.offsetTop;
-    var sectionHeight = section.clientHeight;
-    if (window.scrollY >= sectionTop - sectionHeight / 3) {
-      navbarLinks.forEach(function (navbarLink) {
-        navbarLink.classList.remove("active");
-      });
-      navbarLinks[index].classList.add("active");
+    let sectionTop = section.offsetTop;
+    let sectionBottom = sectionTop + section.clientHeight;
+
+    if (window.scrollY >= sectionTop && window.scrollY < sectionBottom) {
+      currentIndex = index;
+    }
+  });
+
+  navbarLinks.forEach(function (link, index) {
+    if (index === currentIndex) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
     }
   });
 });
+
 // ------------------
 // when click in li ul be display none
 //---------------
 document.addEventListener("DOMContentLoaded", function () {
-  var navbarLinks = document.querySelectorAll("#navbar .navbar-nav .nav-link ");
+  const navbarLinks = document.querySelectorAll(
+    "#navbar .navbar-nav .nav-link"
+  );
   const ul = document.querySelector("#navbarSupportedContent");
-  const close = document.querySelector(".close");
-
-  ul.querySelectorAll("#navbar .navbar-nav .nav-link").forEach((link) => {
+  // Adding click event listeners to each navbar link
+  navbarLinks.forEach((link) => {
     link.addEventListener("click", function () {
+      // Toggle or remove the 'show' class to close the dropdown/menu
       ul.classList.remove("show");
-      console.log("dddddddd");
     });
   });
 });
@@ -66,21 +92,17 @@ document.addEventListener("DOMContentLoaded", function () {
 // navbar hidden when scroll and show when go to 2 section
 // ////////////////////
 window.addEventListener("scroll", function () {
-  var home = document.getElementById("home");
-  var navbar = document.getElementById("navbar");
-  console.log(window.scrollY + "scrollY");
-  console.log(home.offsetTop + "offsetTop");
-  console.log(window.innerHeight + "innerHeight");
-  if (window.scrollY < 10) {
-    navbar.classList.remove("hid");
-    navbar.classList.remove("scrolledBlack");
-    //   } else if (window.scrollY < 700 && window.scrollY > 10) {
-  } else if (
-    window.scrollY < home.offsetTop + window.innerHeight &&
-    window.scrollY > 10
-  ) {
+  const home = document.getElementById("home");
+  const navbar = document.getElementById("navbar");
+  const scrollY = window.scrollY;
+  const homeBottom = home.offsetTop + window.innerHeight;
+
+  if (scrollY < 10) {
+    navbar.classList.remove("hid", "scrolledBlack");
+  } else if (scrollY >= 10 && scrollY < homeBottom) {
     navbar.classList.add("hid");
-  } else if (window.scrollY > home.offsetTop + window.innerHeight) {
+    navbar.classList.remove("scrolledBlack");
+  } else if (scrollY >= homeBottom) {
     navbar.classList.remove("hid");
     navbar.classList.add("scrolledBlack");
   } else {
